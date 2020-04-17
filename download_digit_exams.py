e6ba4008186b3081f241ad8ea8eb7db7bd111ad3
Eero Ruohola <eero@ruohola.dev>
2020-04-18 01:01:46 +0300

    Improve digit exam download script

--- download_digit_exams.py
+++ download_digit_exams.py
@@ -1,36 +1,35 @@
+import os
 import re
+from pathlib import Path
+
 import requests
 from requests.exceptions import RequestException
 
-for i in range(1450,1650):
-    url = f'https://old.digit.fi/viewexam.php?id={i}&download=1'
+filepath = f"{Path.home()}/Downloads/resources"
+os.mkdir(filepath)
+
+
+for i in range(1,2000):
+    url = f"https://old.digit.fi/viewexam.php?id={i}&download=1"
 
     try:
         with requests.get(url) as r:
 
-            fname = ''
+            fname = ""
             if "Content-Disposition" in r.headers.keys():
                 fname = re.findall("filename=(.+)", r.headers["Content-Disposition"])[0]
-                fname = fname[1 : -1]
-                myfile = requests.get(url)
-                fname = fname.replace("/", "-")
-                fname = fname.replace("Ã¤", "ä")
-                fname = fname.replace("Ã¶", "ö")
-                if "iewexam" not in fname:
-                    print(fname)
-                    print(i)
-                    open(f'/Users/werneriaa/Documents/Tentit/{fname}', 'wb').write(myfile.content)
-
             else:
                 fname = url.split("/")[-1]
-                fname = fname[1 : -1]
-                myfile = requests.get(url)
-                fname = fname.replace("/", "-")
-                print(myfile)
-                fname = fname.replace("Ã¤", "ä")
-                fname = fname.replace("Ã¶", "ö")
-                if "iewexam" not in fname:
-                    open(f'/Users/werneriaa/Documents/Tentit/{fname}', 'wb').write(myfile.content)
+            fname = fname[1 : -1]
+            myfile = requests.get(url)
+            fname = fname.replace("/", "-")
+            fname = fname.replace("Ã¤", "a")
+            fname = fname.replace("Ã¶", "o")
+            fname = fname.replace("__", "_")
+            if "iewexam" not in fname:
+                print(i, fname)
+                with open(f"{filepath}/{fname}", "xb") as f:
+                    f.write(myfile.content)
 
-    except RequestException as e:
-        print(e)
+    except (FileExistsError, RequestException) as e:
+        print(str(e))
